b5a758f71e356e1cf62bade05a45fadf552e59a3
Delgersaikhann <deegii@tomujin.digital>
2022-06-14 09:53:23 +0800

    init

---
+++ pages/cameraController.jsx
@@ -0,0 +1,23 @@
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+
+
+export const CameraController = () => {
+    const { camera, gl } = useThree();
+
+    useEffect(
+      () => {
+        const controls = new OrbitControls(camera, gl.domElement);
+  
+        controls.minDistance = 3;
+        controls.maxDistance = 20;
+        return () => {
+          controls.dispose();
+        };
+      },
+      [camera, gl]
+    );
+    return null;
+  };
