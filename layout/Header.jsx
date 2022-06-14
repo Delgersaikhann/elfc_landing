db74f8ecd921a9ed5669b78bad24899fff9d3c6f
temuujinnn <temvkaa@gmail.com>
2022-06-14 19:30:22 +0800

    temka scroll

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -1,9 +1,49 @@
-import {HStack} from "@chakra-ui/react"
+import { Button, HStack, Stack, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 export const Header = () => {
-    return(
-        <HStack minH={"100px"} bg="gray.800" w="100%">
+  const HeaderData = [
+    { page: "home", display: 0 },
+    { page: "About Us", display: 500 },
 
+    { page: "tokenomics", display: 1000 },
+
+    { page: "Whitepaper", display: 1550 },
+  ];
+
+  const HandleClick = (event) => {
+    console.log(event);
+    window.scrollTo({
+      top: event,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <Stack zIndex="1000" pos="fixed" bg="gray.800" w="100%">
+      <HStack p={3} w="70%" mx="auto" justifyContent="space-between">
+        <Text fontSize={"3xl"}>ELF chain</Text>
+        <HStack>
+          {HeaderData.map((el, ind) => {
+            return (
+              //   <Link href={`/${el}`}>
+              <Text
+                cursor="pointer"
+                onClick={() => HandleClick(el.display)}
+                fontSize={"md"}
+                px={5}
+                py={3}
+                key={ind}
+              >
+                {el.page}
+              </Text>
+              //   </Link>
+            );
+          })}
         </HStack>
-    )
-}
+        <Button>Buy ELFc</Button>
+      </HStack>
+    </Stack>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { VStack, Spacer } from "@chakra-ui/react";
+import { VStack, Spacer, Stack, Box } from "@chakra-ui/react";
 import { Header } from "../layout/Header";
 import { Footer } from "../layout/Footer";
 import { TextBox } from "../components/TextBox";
@@ -7,7 +7,14 @@ export default function Home() {
   return (
     <VStack minH="100vh" bg="gray.700" justifyContent="space-between">
       <Header />
+      <Box h="100px" />
       <TextBox />
+      <Stack id="home" h="100vh" bg="red">
+        home
+      </Stack>
+      <Stack id="About Us" h="100vh" bg="red">
+        About Us
+      </Stack>
       {/* <Spacer /> */}
       <Footer />
     </VStack>
